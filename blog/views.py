beef6f00462eb87eb0aa7c6611fde55d3fc200f2
Marina <cendonmarina@gmail.com>
2018-11-17 16:48:44 +0100

    link database with post

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from blog.models import Post
+from django.utils import timezone
 
-# Create your views here.
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'post_list.html', {'posts': posts})
